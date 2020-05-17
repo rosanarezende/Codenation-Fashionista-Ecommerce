502b87fb6800eb094a811778ade23328d861859b
Rosana Rezende <rezende_rosana@hotmail.com>
2020-05-17 18:55:13 -0300

    padding

--- app/src/containers/DetailPage/styles.js
+++ app/src/containers/DetailPage/styles.js
@@ -3,10 +3,11 @@ import styled from 'styled-components'
 export const DetailPageWrapper = styled.div`
     display: flex;
     margin-top: 5vh;
-    padding: 2rem;
+    padding: 2rem 5%;
 
     @media screen and (max-width: 1200px) {
-        flex-direction: column
+        flex-direction: column;
+        padding: 1rem 5%;
     }
 `
 
