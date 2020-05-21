15d5da4439a4c48772ed73b3a22edf54c6cf6d35
Rosana Rezende <rezende_rosana@hotmail.com>
2020-05-21 17:25:36 -0300

    meu primeiro teste

--- app/src/components/DetailImage/index.js
+++ app/src/components/DetailImage/index.js
@@ -7,12 +7,12 @@ function DetailImage(props) {
     return (
         <div className="detail__left">
             {product.image !== ""
-                ? <img className="detail__image" src={product.image} alt={product.name} />
-                : <img className="detail__image" src="https://user-images.githubusercontent.com/45580434/82130318-beceb200-97a0-11ea-82c3-38364a71136a.png" alt={product.name} />
+                ? <img className="detail__image" data-testid="image" src={product.image} alt={product.name} />
+                : <img className="detail__image" data-testid="image" src="https://user-images.githubusercontent.com/45580434/82130318-beceb200-97a0-11ea-82c3-38364a71136a.png" alt={product.name} />
             }
 
             {product.on_sale &&
-                <div className="detail__stamp">{product.discount_percentage}</div>
+                <div className="detail__stamp" data-testid="stamp" >{product.discount_percentage}</div>
             }
         </div>
     )
