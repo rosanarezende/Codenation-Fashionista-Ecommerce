a591a9d2199554b9ba0686ee0b91ef8dd7dac086
Rosana Rezende <rezende_rosana@hotmail.com>
2020-05-21 17:34:27 -0300

    mocando produtos para testes

--- app/src/components/DetailImage/index.test.js
+++ app/src/components/DetailImage/index.test.js
@@ -1,61 +1,23 @@
 import React from "react";
 import { render } from "@testing-library/react"
-
+import { products } from "../../utils/mock"
 import DetailImage from "./index"
 
-const mockProduct = {
-    "name": "T-SHIRT LEATHER DULL",
-    "style": "20002602",
-    "code_color": "20002602_027",
-    "color_slug": "marinho",
-    "color": "MARINHO",
-    "on_sale": true,
-    "regular_price": "R$ 139,90",
-    "actual_price": "R$ 119,90",
-    "discount_percentage": "12%",
-    "installments": "3x R$ 39,97",
-    "image": "",
-    "sizes": [
-        {
-            "available": true,
-            "size": "PP",
-            "sku": "5793_1000032_0_PP"
-        },
-        {
-            "available": true,
-            "size": "P",
-            "sku": "5793_1000032_0_P"
-        },
-        {
-            "available": true,
-            "size": "M",
-            "sku": "5793_1000032_0_M"
-        },
-        {
-            "available": false,
-            "size": "G",
-            "sku": "5793_1000032_0_G"
-        },
-        {
-            "available": false,
-            "size": "GG",
-            "sku": "5793_1000032_0_GG"
-        }
-    ]
-}
+const mockProduct = products[4]
 
-describe("DetailImage", () => {    
+describe("DetailImage", () => {
+    let getByTestId
+    beforeEach(() => {
+        const view = render(<DetailImage product={mockProduct} />)
+        getByTestId = view.getByTestId
+    })
     
     it("should render IMAGE correctly", async () => {
-        const { getByTestId}  = render(<DetailImage product={mockProduct} />)
         const image = getByTestId("image")        
-
         expect(image).toHaveClass("detail__image")
     })
 
-    it("should render STAMP correctly", async () => {
-        const { getByTestId } = render(<DetailImage product={mockProduct} />)
-        
+    it("should render STAMP correctly", async () => {       
         // só tem stamp em produto em promoção
         if(mockProduct.on_sale){
             const stamp = getByTestId("stamp")
---
+++ app/src/utils/mock.js
@@ -0,0 +1,202 @@
+export const products = [
+    {
+        "name": "VESTIDO TRANSPASSE BOW",
+        "style": "20002605",
+        "code_color": "20002605_613",
+        "color_slug": "tapecaria",
+        "color": "TAPEÇARIA",
+        "on_sale": false,
+        "regular_price": "R$ 199,90",
+        "actual_price": "R$ 199,90",
+        "discount_percentage": "",
+        "installments": "3x R$ 66,63",
+        "image": "https://viniciusvinna.netlify.app/assets/api-fashionista/20002605_615_catalog_1.jpg",
+        "sizes": [
+            {
+                "available": false,
+                "size": "PP",
+                "sku": "5807_343_0_PP"
+            },
+            {
+                "available": true,
+                "size": "P",
+                "sku": "5807_343_0_P"
+            },
+            {
+                "available": true,
+                "size": "M",
+                "sku": "5807_343_0_M"
+            },
+            {
+                "available": true,
+                "size": "G",
+                "sku": "5807_343_0_G"
+            },
+            {
+                "available": false,
+                "size": "GG",
+                "sku": "5807_343_0_GG"
+            }
+        ]
+    },
+    {
+        "name": "REGATA ALCINHA FOLK",
+        "style": "20002570",
+        "code_color": "20002570_614",
+        "color_slug": "preto",
+        "color": "PRETO",
+        "on_sale": false,
+        "regular_price": "R$ 99,90",
+        "actual_price": "R$ 99,90",
+        "discount_percentage": "",
+        "installments": "3x R$ 33,30",
+        "image": "https://viniciusvinna.netlify.app/assets/api-fashionista/20002570_002_catalog_1.jpg",
+        "sizes": [
+            {
+                "available": true,
+                "size": "PP",
+                "sku": "5723_40130843_0_PP"
+            },
+            {
+                "available": true,
+                "size": "P",
+                "sku": "5723_40130843_0_P"
+            },
+            {
+                "available": true,
+                "size": "M",
+                "sku": "5723_40130843_0_M"
+            },
+            {
+                "available": true,
+                "size": "G",
+                "sku": "5723_40130843_0_G"
+            },
+            {
+                "available": true,
+                "size": "GG",
+                "sku": "5723_40130843_0_GG"
+            }
+        ]
+    },
+    {
+        "name": "TOP CROPPED SUEDE",
+        "style": "20002575",
+        "code_color": "20002575_035",
+        "color_slug": "caramelo",
+        "color": "CARAMELO",
+        "on_sale": false,
+        "regular_price": "R$ 129,90",
+        "actual_price": "R$ 129,90",
+        "discount_percentage": "",
+        "installments": "3x R$ 43,30",
+        "image": "https://viniciusvinna.netlify.app/assets/api-fashionista/20002575_027_catalog_1.jpg",
+        "sizes": [
+            {
+                "available": false,
+                "size": "PP",
+                "sku": "5733_1000054_0_PP"
+            },
+            {
+                "available": true,
+                "size": "P",
+                "sku": "5733_1000054_0_P"
+            },
+            {
+                "available": true,
+                "size": "M",
+                "sku": "5733_1000054_0_M"
+            },
+            {
+                "available": true,
+                "size": "G",
+                "sku": "5733_1000054_0_G"
+            },
+            {
+                "available": false,
+                "size": "GG",
+                "sku": "5733_1000054_0_GG"
+            }
+        ]
+    },
+    {
+        "name": "BATA DECOTE FLUID",
+        "style": "20002581",
+        "code_color": "20002581_614",
+        "color_slug": "mini-folk",
+        "color": "MINI FOLK",
+        "on_sale": false,
+        "regular_price": "R$ 149,90",
+        "actual_price": "R$ 149,90",
+        "discount_percentage": "",
+        "installments": "3x R$ 49,97",
+        "image": "https://viniciusvinna.netlify.app/assets/api-fashionista/20002581_614_catalog_1.jpg",
+        "sizes": [
+            {
+                "available": false,
+                "size": "PP",
+                "sku": "5749_341_0_PP"
+            },
+            {
+                "available": true,
+                "size": "P",
+                "sku": "5749_341_0_P"
+            },
+            {
+                "available": false,
+                "size": "M",
+                "sku": "5749_341_0_M"
+            },
+            {
+                "available": true,
+                "size": "G",
+                "sku": "5749_341_0_G"
+            },
+            {
+                "available": true,
+                "size": "GG",
+                "sku": "5749_341_0_GG"
+            }
+        ]
+    },
+    {
+        "name": "T-SHIRT LEATHER DULL",
+        "style": "20002602",
+        "code_color": "20002602_027",
+        "color_slug": "marinho",
+        "color": "MARINHO",
+        "on_sale": true,
+        "regular_price": "R$ 139,90",
+        "actual_price": "R$ 119,90",
+        "discount_percentage": "12%",
+        "installments": "3x R$ 39,97",
+        "image": "",
+        "sizes": [
+            {
+                "available": true,
+                "size": "PP",
+                "sku": "5793_1000032_0_PP"
+            },
+            {
+                "available": true,
+                "size": "P",
+                "sku": "5793_1000032_0_P"
+            },
+            {
+                "available": true,
+                "size": "M",
+                "sku": "5793_1000032_0_M"
+            },
+            {
+                "available": false,
+                "size": "G",
+                "sku": "5793_1000032_0_G"
+            },
+            {
+                "available": false,
+                "size": "GG",
+                "sku": "5793_1000032_0_GG"
+            }
+        ]
+    }
+]
