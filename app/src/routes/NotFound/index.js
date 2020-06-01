cfcfb94f873a0555be48d6793710fc55c6ad145a
Rosana Rezende <rezende_rosana@hotmail.com>
2020-05-31 22:57:22 -0300

    trocando connect por useDispatch

--- app/src/routes/NotFound/index.js
+++ app/src/routes/NotFound/index.js
@@ -1,24 +1,22 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { push } from 'connected-react-router'
 import { routes } from "../../utils/constants"
 
 import './index.css'
 
-function NotFound(props) {
-    const { goToHome } = props
+function NotFound() {
+    const dispatch = useDispatch()
+    const goToHome = push(routes.home)
+
     return (
         <div className="not-found">
             <h3>Página não encontrada</h3>
-            <p onClick={goToHome} className="not-found__link">
+            <p onClick={() => dispatch(goToHome)} className="not-found__link">
                 <i className="fa fa-arrow-left" aria-hidden="true"/> Voltar para Home
             </p>
         </div>
     )
 }
 
-const mapDispatchToProps = dispatch => ({
-    goToHome: () => dispatch(push(routes.home))
-})
-
-export default connect(null, mapDispatchToProps)(NotFound)
+export default NotFound
